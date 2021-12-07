810e0691d580dcb97b2ae5bde53394c6de9fa101
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-07 16:51:09 +0530

    create view include header foooter partials ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,11 @@
 
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is running for the  chatapp</h1>')
+
+    return res.render('home',{
+       title:"Home"
+    });
+//     return res.end('<h1>Express is running for the  chatapp</h1>')
+// 
 }
 
 
