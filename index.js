6c0dcd0fecc369216232afda2e8503c9619ba8a9
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-07 10:57:05 +0530

    set up ejs view engine

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use('/',require('./routes'));
 
 
 
+//set up view engine 
+app.set('view engine','ejs');
+app.set('views', './views');
+
+
+
 app.listen(port,function(err){
 if(err){
     console.log('error',error in running);
