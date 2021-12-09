5c9f15d93c742c0228f1e5784e37b2608a5bee61
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-09 09:13:27 +0530

    set up user sign up and sign

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,16 @@ module.exports.profile = function(req,res){
         title:"user profile"
      });
 }
+
+//render sign up
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"chatapp| Sign Up"
+    })
+}
+// render sign in
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"chatapp|Sign in"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,7 @@ const router = expres.Router();
 const userController = require('../controllers/users_controller');
 router.get('/profile', userController.profile);
 
+router.get('/sign-up',userController.signUp);
+router.get('/sign-in',userController.signIn);
+
 module.exports = router;
