af4b4ee693550e1ce24f3f3d9cdbb36137023c04
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-15 13:05:22 +0530

    check on user authentication before post

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 const expres =require('express');
 const router = expres.Router();
+const passport = require('passport');
 
 
 const postController = require('../controllers/posts_controller');
 
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication, postController.create);
 
 
 module.exports = router;
