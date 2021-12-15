58d358d82d330328ecfd3b8015e06cba462281f5
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-15 12:35:53 +0530

    display post on home and posets user by pre- populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,42 @@
-
+const Post = require('../models/post');
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home',{
-       title:"Home"
-    });
-//     return res.end('<h1>Express is running for the  chatapp</h1>')
-// 
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+
+    //  Post.find({},function(err,posts){
+       
+
+    //     return res.render('home',{
+    //         title:"ChatApp | Home",
+    //         posts: posts
+
+
+    //  });
+
+    
+    // });
+
+    // used populate to show specific name from whole user data
+    //exec()  in longer query seprate query and call back 
+
+    Post.find({}).populate('user').exec(function(err,posts){
+
+        return res.render('home',{
+            title:"ChatApp | Home",
+            posts: posts
+
+
+     });
+
+    })
+
+
 }
+
+//     return res.end('<h1>Express is running for the  chatapp</h1>')
+
+
 
 
 
