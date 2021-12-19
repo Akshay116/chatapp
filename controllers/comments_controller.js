423b7813363f4e0cdcc62977bc5e00b95d6705e0
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-19 17:11:28 +0530

    deleting comment if signed in  comment user complted

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,3 +1,4 @@
+const { redirect } = require('express/lib/response');
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
@@ -29,3 +30,25 @@ module.exports.create = function(req,res){
       });
 
 }
+
+module.exports.destroy = function(req,res){
+  Comment.findById(req.params.id,function(err,comment){
+      if(comment.user == req.user.id){
+
+         let postId = comment.post;//to update it from database of post to 
+
+         comment.remove();
+
+         Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+           return res.redirect('back');
+         });
+
+      }else{
+            return res.redirect('back');
+
+      }
+
+  });
+}
+
+
