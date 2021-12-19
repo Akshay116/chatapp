f47057f349fdbf3e30a51cc3281d155d35720e81
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2021-12-19 20:49:39 +0530

    update user profile of logged in user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -32,13 +33,19 @@ module.exports.home = function(req,res){
     
     .exec(function(err,posts){
 
-        return res.render('home',{
-            title:"ChatApp | Home",
-            posts: posts
+        User.find({},function(err,users){
+            return res.render('home',{
+                title:"ChatApp | Home",
+                posts: posts,
+                all_users: users
+    
+    
+         });
+    
 
+        });
 
-     });
-
+        
     })
 
 
